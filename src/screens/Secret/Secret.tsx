8477971c62a10dcffc92cef408e62240919a91ad
crusaderincode <crusaderincode@proton.me>
2023-03-16 23:32:30 +0300

    basic setup

---
+++ src/screens/Secret/Secret.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from 'react-native';
+
+const Secret = () => {
+  return (
+    <View className="flex-1 items-center  justify-center bg-white">
+      <Text className="text-red-500 text-2xl mx-5 text-center">Secret page of my app</Text>
+    </View>
+  );
+}
+
+
+export default Secret
+
---
+++ src/screens/Weather/Weather.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from 'react-native';
+
+const Weather = () => {
+  return (
+    <View className="flex-1 items-center  justify-center bg-white">
+      <Text className="text-red-500 text-2xl mx-5 text-center">Weather page of my app</Text>
+    </View>
+  );
+}
+
+
+export default Weather
+
