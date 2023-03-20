46f5618f3ea14d900d0abff83fe6b8cf0cea9168
crusaderincode <crusaderincode@proton.me>
2023-03-20 14:00:03 +0300

    husky added

--- App.tsx
+++ App.tsx
@@ -1,20 +1,17 @@
-import { Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Secret from './src/screens/Secret/Secret';
-import Weather from './src/screens/Weather/Weather';
+import { NavigationContainer } from "@react-navigation/native"
+import { createNativeStackNavigator } from "@react-navigation/native-stack"
+import Secret from "./src/screens/Secret/Secret"
+import Weather from "./src/screens/Weather/Weather"
 
-const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator()
 
 export default function App() {
   return (
     <NavigationContainer>
-    <Stack.Navigator>
-    <Stack.Screen name="Weather" component={Weather} />
-    <Stack.Screen name="Secret" component={Secret} />
+      <Stack.Navigator>
+        <Stack.Screen name="Weather" component={Weather} />
+        <Stack.Screen name="Secret" component={Secret} />
       </Stack.Navigator>
-      </NavigationContainer>
-  );
+    </NavigationContainer>
+  )
 }
-
-
--- src/screens/Secret/Secret.tsx
+++ src/screens/Secret/Secret.tsx
@@ -1,13 +1,14 @@
-import { Text, View } from 'react-native';
+import React from "react"
+import { Text, View } from "react-native"
 
 const Secret = () => {
   return (
     <View className="flex-1 items-center  justify-center bg-white">
-      <Text className="text-red-500 text-2xl mx-5 text-center">Secret page of my app</Text>
+      <Text className="text-red-500 text-2xl mx-5 text-center">
+        Secret page of my app
+      </Text>
     </View>
-  );
+  )
 }
 
-
 export default Secret
-
